ef9c90d7cde2a1a533527df3fd7a485b43ce8982
shuting_chang <37363421+shuuuuting@users.noreply.github.com>
2020-07-19 13:23:28 +0800

    Add files via upload

---
+++ code/src/StockTest.java
@@ -0,0 +1,28 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zhangshuting
+ */
+public class StockTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginForm lgf = new LoginForm();
+        lgf.setVisible(true);
+        lgf.pack();
+        lgf.setLocationRelativeTo(null);
+        lgf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+    
+}
